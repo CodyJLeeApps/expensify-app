c0bca77da38aa7d00418af766f6a96468684c7fd
Cody <cody.j.lee@icloud.com>
2020-02-29 16:42:27 -0500

    finishing up destructuring experiments

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,34 +1,44 @@
-// object destructuring
-const person = {
-    name: 'Andrew',
-    age: 26,
-    location: {
-        city: 'CLT',
-        temp: 92
-    }
-};
+// // object destructuring
+// const person = {
+//     name: 'Andrew',
+//     age: 26,
+//     location: {
+//         city: 'CLT',
+//         temp: 92
+//     }
+// };
 
-const { name = 'Anonymous', age } = person;
+// const { name = 'Anonymous', age } = person;
 
-// const name = person.name;
-// const age = person.age;
+// // const name = person.name;
+// // const age = person.age;
 
-console.log(`${name} is ${age}.`);
+// console.log(`${name} is ${age}.`);
 
-const {city, temp: temperature} = person.location;
-if(city && temperature) {
-    console.log(`It's ${temperature} in ${city}`);
-}
+// const {city, temp: temperature} = person.location;
+// if(city && temperature) {
+//     console.log(`It's ${temperature} in ${city}`);
+// }
 
-const book = {
-    title: 'Book Name',
-    author: 'tbd tbd',
-    publisher: {
-        name: 'penguin'
-    }
-};
+// const book = {
+//     title: 'Book Name',
+//     author: 'tbd tbd',
+//     publisher: {
+//         name: 'penguin'
+//     }
+// };
 
-const {name: publisherName = 'self-published'} = book.publisher
-console.log(publisherName);
+// const {name: publisherName = 'self-published'} = book.publisher
+// console.log(publisherName);
 
 // array destructuring
+const address = ['12299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19147'];
+
+const [street, city, state = 'Unknown', zip] = address;
+
+console.log(`You are in ${city} ${state}.`);
+
+const item = ['coffee (hot)', '$2.00', '$2.50', '$2.75'];
+
+const [name, smallPrice, medPrice, largePrice] = item;
+console.log(`A Med. ${name} costs ${medPrice}`);
