03d3447c701c7824bd764d55b2bc59a1c01196bd
Cody <cody.j.lee@icloud.com>
2020-03-07 20:42:44 -0500

    Adding array sorting

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -131,6 +131,12 @@ const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
         const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
 
         return startDateMatch && endDateMatch && textMatch;
+    }).sort((a, b) => {
+        if(sortBy === 'date') {
+            return a.createdAt < b.createdAt ? 1 : -1;
+        } else if(sortBy === 'amount') {
+            return a.amount < b.amount ? 1 : -1;
+        }
     });
 };
 
@@ -148,14 +154,14 @@ store.subscribe(() => {
     console.log(visibleExpenses);
 });
 
-const expense1 = store.dispatch(addExpense({description: 'Rent', amount: 100, createdAt: 1000 }));
+const expense1 = store.dispatch(addExpense({description: 'Rent', amount: 100, createdAt: -21000 }));
 const expense2 = store.dispatch(addExpense({description: 'Coffee', amount: 300, createdAt: -1000 }));
 //store.dispatch(removeExpense({ id: expense1.expense.id }));
 // store.dispatch(editExpense( expense2.expense.id, { amount: 500 }));
-store.dispatch(setTextFilter('Rent'));
-// store.dispatch(sortByAmount());
-// store.dispatch(sortByDate());
-store.dispatch(setStartDate(100));
+//store.dispatch(setTextFilter('Rent'));
+store.dispatch(sortByAmount());
+//store.dispatch(sortByDate());
+//store.dispatch(setStartDate(100));
 //store.dispatch(setStartDate(1250));
 //store.dispatch(setEndDate(1250));
 
