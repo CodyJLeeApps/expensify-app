9901871c52b987f520e5637bdd91939efec5f869
Cody <cody.j.lee@icloud.com>
2020-03-07 21:10:19 -0500

    Creating the store in a seperate file, as well as updating webpack to use the actual app.js file

---
+++ src/selectors/expenses.js
@@ -0,0 +1,18 @@
+// Get visible expenses
+const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
+    return expenses.filter((expense) => {
+        const startDateMatch = typeof startDate !== 'number' || expenses.createdAt >= startDate;
+        const endDateMatch = typeof endDate !== 'number' || expenses.endDate <= endDate;
+        const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
+
+        return startDateMatch && endDateMatch && textMatch;
+    }).sort((a, b) => {
+        if(sortBy === 'date') {
+            return a.createdAt < b.createdAt ? 1 : -1;
+        } else if(sortBy === 'amount') {
+            return a.amount < b.amount ? 1 : -1;
+        }
+    });
+};
+
+export default getVisibleExpenses;
