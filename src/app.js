6e7ddd3549f1000b710d9590b83743930a52725c
Cody <cody.j.lee@icloud.com>
2020-03-22 15:06:38 -0400

    Adding a dropdown to allow modification of sorting method.

--- src/app.js
+++ src/app.js
@@ -13,12 +13,8 @@ import './styles/styles.scss';
 const store = configureStore();
 
 store.dispatch(addExpense({ description: 'Water Bill', amount: 4500 }));
-store.dispatch(addExpense({ description: 'Gas Bill', amount: 9500}));
-store.dispatch(setTextFilter('water'));
-
-setTimeout(() => {
-    store.dispatch(setTextFilter('Bill'));
-}, 3000)
+store.dispatch(addExpense({ description: 'Gas Bill', amount: 0, createdAt: 1000}));
+store.dispatch(addExpense({ description: 'Rent', amount: 109500}));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
