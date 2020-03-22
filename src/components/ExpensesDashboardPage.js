71907ac2484b4ad33e41680dbe3d1cc12bb8cd9a
Cody <cody.j.lee@icloud.com>
2020-03-22 14:50:10 -0400

    Adding a expense filter text field, connected it to the store, as well as added a remove expense button connected to the store

--- src/components/ExpensesDashboardPage.js
+++ src/components/ExpensesDashboardPage.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
+import ExpenseLIstFilters from './ExpenseListFilters';
+import ExpenseListFilters from './ExpenseListFilters';
 
 const ExpenseDashboardPage = () => (
     <div>
+        <ExpenseListFilters />
         <ExpenseList />
     </div>
 );
