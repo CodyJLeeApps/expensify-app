7d2dde488d3f6800b9ad116f959689d1bd6a3df1
Cody <cody.j.lee@icloud.com>
2020-03-28 14:33:57 -0400

    Adding ability to create new expenses and re-directing page when you do.

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -9,7 +9,8 @@ export default class ExpenseForm extends React.Component {
         note: '',
         amount: '',
         createdAt: moment(),
-        calendarFocused: false
+        calendarFocused: false,
+        error: ''
     };
 
     onDescriptionhange = (e) => {
@@ -25,23 +26,42 @@ export default class ExpenseForm extends React.Component {
     onAmountChange = (e) => {
         const amount = e.target.value;
         
-        if(amount.match(/^\d*(\.\d{0,2})?$/)) {
+        if(!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
             this.setState(() => ({ amount }));
         }
     };
 
     onDateChange = (createdAt) => {
-        this.setState(() => ({ createdAt }));
+        if(createdAt) {
+            this.setState(() => ({ createdAt }));
+        }
     };
 
     onFocusChange = ({ focused }) => {
         this.setState(() => ({ calendarFocused: focused }));
     };
 
+    onSubmit = (e) => {
+        e.preventDefault();
+
+        if(!this.state.description || !this.state.amount) {
+            this.setState(() => ({ error: 'Please provide a description and Amount!' }));
+        } else {
+            this.setState(() => ({ error: '' }));
+            this.props.onSubmit({
+                description: this.state.description,
+                amount: parseFloat(this.state.amount)*100,
+                createdAt: this.state.createdAt.valueOf(),
+                note: this.state.note
+            });
+        }
+    };
+
     render() {
         return (
             <div>
-                <form>
+                {this.state.error && <p>{this.state.error}</p>}
+                <form onSubmit={this.onSubmit}>
                     <input 
                         type="text"
                         placeholder="Description"
